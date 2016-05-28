d8caf11712b922a7f96f8317e2344e86f9d129bc
sergeys <sergeys@2213d24a-d591-11e5-a0f6-fbcad358b9ab>
2016-05-28 11:59:14 +0000

    sd_card old test with sources

---
+++ EEar/soft/tests/25_sdcard_mic/stm324xg_eval.h
@@ -0,0 +1,229 @@
+/**
+  ******************************************************************************
+  * @file    stm324xg_eval.h
+  * @author  MCD Application Team
+  * @version V1.1.1
+  * @date    11-January-2013
+  * @brief   This file contains definitions for STM324xG_EVAL's Leds, push-buttons
+  *          and COM ports hardware resources.
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; COPYRIGHT 2013 STMicroelectronics</center></h2>
+  *
+  * Licensed under MCD-ST Liberty SW License Agreement V2, (the "License");
+  * You may not use this file except in compliance with the License.
+  * You may obtain a copy of the License at:
+  *
+  *        http://www.st.com/software_license_agreement_liberty_v2
+  *
+  * Unless required by applicable law or agreed to in writing, software 
+  * distributed under the License is distributed on an "AS IS" BASIS, 
+  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  * See the License for the specific language governing permissions and
+  * limitations under the License.
+  *
+  ******************************************************************************
+  */ 
+  
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __STM324xG_EVAL_H
+#define __STM324xG_EVAL_H
+
+#ifdef __cplusplus
+ extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f4xx.h"
+   
+/** @addtogroup Utilities
+  * @{
+  */
+
+/** @addtogroup STM32_EVAL
+  * @{
+  */
+
+/** @addtogroup STM324xG_EVAL
+  * @{
+  */
+      
+/** @addtogroup STM324xG_EVAL_LOW_LEVEL
+  * @{
+  */ 
+
+/** @defgroup STM324xG_EVAL_LOW_LEVEL_Exported_Types
+  * @{
+  */
+typedef enum 
+{
+  LED1 = 0,
+  LED2 = 1,
+  LED3 = 2,
+  LED4 = 3
+} Led_TypeDef;
+
+
+/**
+  * @}
+  */ 
+
+/** @defgroup STM324xG_EVAL_LOW_LEVEL_Exported_Constants
+  * @{
+  */ 
+
+/** 
+  * @brief  Define for STM324xG_EVAL board  
+  */ 
+#if !defined (USE_STM324xG_EVAL)
+ #define USE_STM324xG_EVAL
+#endif
+
+/** @addtogroup STM324xG_EVAL_LOW_LEVEL_LED
+  * @{
+  */
+#define LEDn                             4
+
+#define LED1_PIN                         GPIO_Pin_12
+#define LED1_GPIO_PORT                   GPIOD
+#define LED1_GPIO_CLK                    RCC_AHB1Periph_GPIOD  
+  
+#define LED2_PIN                         GPIO_Pin_13
+#define LED2_GPIO_PORT                   GPIOD
+#define LED2_GPIO_CLK                    RCC_AHB1Periph_GPIOD  
+  
+#define LED3_PIN                         GPIO_Pin_14
+#define LED3_GPIO_PORT                   GPIOD
+#define LED3_GPIO_CLK                    RCC_AHB1Periph_GPIOD  
+  
+#define LED4_PIN                         GPIO_Pin_15
+#define LED4_GPIO_PORT                   GPIOD
+#define LED4_GPIO_CLK                    RCC_AHB1Periph_GPIOD
+
+/**
+  * @}
+  */ 
+  
+/** @addtogroup STM324xG_EVAL_LOW_LEVEL_BUTTON
+  * @{
+  */  
+
+/**
+  * @}
+  */ 
+
+/** @addtogroup STM324xG_EVAL_LOW_LEVEL_COM
+  * @{
+  */
+/**
+  * @}
+  */ 
+
+/** @addtogroup STM324xG_EVAL_LOW_LEVEL_SD_FLASH
+  * @{
+  */ 
+/**
+  * @brief  SD FLASH SDIO Interface
+  */
+#define SD_DETECT_PIN                    GPIO_Pin_12
+#define SD_DETECT_GPIO_PORT              GPIOB
+#define SD_DETECT_GPIO_CLK               RCC_AHB1Periph_GPIOB
+   
+#define SDIO_FIFO_ADDRESS                ((uint32_t)0x40012C80)
+/** 
+  * @brief  SDIO Intialization Frequency (400KHz max)
+  */
+#define SDIO_INIT_CLK_DIV                ((uint8_t)0x76)
+/** 
+  * @brief  SDIO Data Transfer Frequency (25MHz max) 
+  */
+#define SDIO_TRANSFER_CLK_DIV            ((uint8_t)0x0) 
+
+#define SD_SDIO_DMA                   DMA2
+#define SD_SDIO_DMA_CLK               RCC_AHB1Periph_DMA2
+ 
+#define SD_SDIO_DMA_STREAM3	          3
+//#define SD_SDIO_DMA_STREAM6           6
+
+#ifdef SD_SDIO_DMA_STREAM3
+ #define SD_SDIO_DMA_STREAM            DMA2_Stream3
+ #define SD_SDIO_DMA_CHANNEL           DMA_Channel_4
+ #define SD_SDIO_DMA_FLAG_FEIF         DMA_FLAG_FEIF3
+ #define SD_SDIO_DMA_FLAG_DMEIF        DMA_FLAG_DMEIF3
+ #define SD_SDIO_DMA_FLAG_TEIF         DMA_FLAG_TEIF3
+ #define SD_SDIO_DMA_FLAG_HTIF         DMA_FLAG_HTIF3
+ #define SD_SDIO_DMA_FLAG_TCIF         DMA_FLAG_TCIF3 
+ #define SD_SDIO_DMA_IRQn              DMA2_Stream3_IRQn
+ #define SD_SDIO_DMA_IRQHANDLER        DMA2_Stream3_IRQHandler 
+#elif defined SD_SDIO_DMA_STREAM6
+ #define SD_SDIO_DMA_STREAM            DMA2_Stream6
+ #define SD_SDIO_DMA_CHANNEL           DMA_Channel_4
+ #define SD_SDIO_DMA_FLAG_FEIF         DMA_FLAG_FEIF6
+ #define SD_SDIO_DMA_FLAG_DMEIF        DMA_FLAG_DMEIF6
+ #define SD_SDIO_DMA_FLAG_TEIF         DMA_FLAG_TEIF6
+ #define SD_SDIO_DMA_FLAG_HTIF         DMA_FLAG_HTIF6
+ #define SD_SDIO_DMA_FLAG_TCIF         DMA_FLAG_TCIF6 
+ #define SD_SDIO_DMA_IRQn              DMA2_Stream6_IRQn
+ #define SD_SDIO_DMA_IRQHANDLER        DMA2_Stream6_IRQHandler
+#endif /* SD_SDIO_DMA_STREAM3 */
+
+/**
+  * @}
+  */ 
+  
+/** @addtogroup STM324xG_EVAL_LOW_LEVEL_I2C_EE
+  * @{
+  */
+/**
+  * @}
+  */  
+/**
+  * @}
+  */ 
+  
+/** @defgroup STM324xG_EVAL_LOW_LEVEL_Exported_Macros
+  * @{
+  */  
+/**
+  * @}
+  */ 
+
+
+/** @defgroup STM324xG_EVAL_LOW_LEVEL_Exported_Functions
+  * @{
+  */
+void STM_EVAL_LEDInit(Led_TypeDef Led);
+void STM_EVAL_LEDOn(Led_TypeDef Led);
+void STM_EVAL_LEDOff(Led_TypeDef Led);
+void STM_EVAL_LEDToggle(Led_TypeDef Led);
+void SD_LowLevel_DeInit(void);
+void SD_LowLevel_Init(void); 
+void SD_LowLevel_DMA_TxConfig(uint32_t *BufferSRC, uint32_t BufferSize);
+void SD_LowLevel_DMA_RxConfig(uint32_t *BufferDST, uint32_t BufferSize);
+/**
+  * @}
+  */
+  
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __STM324xG_EVAL_H */
+/**
+  * @}
+  */ 
+
+/**
+  * @}
+  */ 
+
+/**
+  * @}
+  */
+
+/**
+  * @}
+  */  
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
