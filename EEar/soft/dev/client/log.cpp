8648fad41cfe7b395099843c05d8e21f703c6bb3
Sergey <victorovich2004@users.noreply.github.com>
2016-08-08 15:26:18 +0000

    client: log without file I/O possible

--- EEar/soft/dev/client/log.cpp
+++ EEar/soft/dev/client/log.cpp
@@ -5,7 +5,7 @@
 
 CLog::CLog(const char *filename,bool stdout_echo)
 {
-  strcpy(m_filename,filename);
+  strcpy(m_filename,filename?filename:"");
   b_stdout_echo = stdout_echo;
 }
 
@@ -30,12 +30,15 @@ void CLog::Add(const char *format,...)
 
   strcat(s,"\r\n");
 
-  FIL f;
-  if ( f_open(&f,m_filename,FA_WRITE|FA_OPEN_APPEND) == FR_OK )
+  if ( m_filename[0] )
      {
-       UINT wb = 0;
-       f_write(&f,s,strlen(s),&wb);
-       f_close(&f);
+       FIL f;
+       if ( f_open(&f,m_filename,FA_WRITE|FA_OPEN_APPEND) == FR_OK )
+          {
+            UINT wb = 0;
+            f_write(&f,s,strlen(s),&wb);
+            f_close(&f);
+          }
      }
 
   if ( b_stdout_echo )
