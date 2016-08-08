e5ebea4504fe191d40d2a83df66188962a6b0cde
Sergey <victorovich2004@users.noreply.github.com>
2016-08-08 15:29:45 +0000

    client: bug in RTC sync fixed, IRQ Timestamp added

--- EEar/soft/dev/client/rtc.h
+++ EEar/soft/dev/client/rtc.h
@@ -5,22 +5,36 @@
 
 class CRTC
 {
+  public:
+          typedef void (*TCALLBACK)(void*,OURTIME ts_unshifted,OURTIME ts_shifted);
+
+  private:        
           static const int SUBSECONDS_BITS = 10;  // to get 1/1024 sec resolution (min is 8 bits, max is 15)
 
           static volatile OURTIME m_shift;
 
+          static TCALLBACK p_cb;
+          static void *p_cbparm;
+
   public:
-          static bool Init(char yy,char mm,char dd,char wd,char hh,char nn,char ss);
+          static bool Init(char yy,char mm,char dd,char wd,char hh,char nn,char ss,
+                           TCALLBACK cbts=NULL,void *cbparm=NULL,int irq_priority=2);
 
           static void SetShift(OURTIME shift);
+          static OURTIME GetShift(); // for debug
 
-          static OURTIME GetTime(bool use_shift=true);
-          static bool GetTS(OURTIME& _time,bool use_shift=false);
+          static OURTIME GetTime(); // WARNING!!! it waits for 1/32768 sec, not thread-safe! returns shifted time
+          
+          static bool GetTS(OURTIME& _time,bool use_shift=false);  // use it for polling mode only (non-interrupt!)
 
   private:
           static void GetTime(char& yy,char& mm,char& dd,char& wd,char& hh,char& nn,char& ss,int& subsec);
           static bool GetTS(char& yy,char& mm,char& dd,char& wd,char& hh,char& nn,char& ss,int& subsec);
           static int SS2MS(int subs);
+
+  public:
+          // used internally:
+          static void OnIRQ_Internal();
 
 };
 
