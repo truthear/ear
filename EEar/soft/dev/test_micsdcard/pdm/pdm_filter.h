0b6dccd8111ccec3fe1123e7be6b14d1c5844b53
sergeys <sergeys@2213d24a-d591-11e5-a0f6-fbcad358b9ab>
2016-08-20 10:07:00 +0000

    old tests pending commit made

--- EEar/soft/dev/test_micsdcard/pdm/pdm_filter.h
+++ EEar/soft/dev/test_micsdcard/pdm/pdm_filter.h
@@ -61,10 +61,10 @@ void PDM_Filter_Init(PDMFilter_InitStruct * Filter);
 
 int32_t PDM_Filter_64_MSB(uint8_t* data, uint16_t* dataOut, uint16_t MicGain, PDMFilter_InitStruct * Filter);
 int32_t PDM_Filter_80_MSB(uint8_t* data, uint16_t* dataOut, uint16_t MicGain, PDMFilter_InitStruct * Filter);
-int32_t PDM_Filter_128_MSB(uint8_t* data, uint16_t* dataOut, uint16_t MicGain, PDMFilter_InitStruct * Filter);
+//int32_t PDM_Filter_128_MSB(uint8_t* data, uint16_t* dataOut, uint16_t MicGain, PDMFilter_InitStruct * Filter);
 int32_t PDM_Filter_64_LSB(uint8_t* data, uint16_t* dataOut, uint16_t MicGain, PDMFilter_InitStruct * Filter);
 int32_t PDM_Filter_80_LSB(uint8_t* data, uint16_t* dataOut, uint16_t MicGain, PDMFilter_InitStruct * Filter);
-int32_t PDM_Filter_128_LSB(uint8_t* data, uint16_t* dataOut, uint16_t MicGain, PDMFilter_InitStruct * Filter);
+//int32_t PDM_Filter_128_LSB(uint8_t* data, uint16_t* dataOut, uint16_t MicGain, PDMFilter_InitStruct * Filter);
 
 #ifdef __cplusplus
 }
