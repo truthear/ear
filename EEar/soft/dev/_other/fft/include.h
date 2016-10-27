12e8e5f30e2420e292b3df646ea53e01cd1912d3
Sergey <victorovich2004@users.noreply.github.com>
2016-10-27 08:18:01 +0000

    text analyzer added

---
+++ EEar/soft/dev/_other/fft/include.h
@@ -0,0 +1,28 @@
+
+#include <windows.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <string>
+#include <vector>
+#include "fdetector.h"
+
+#define MAX(a,b)   ((a)>(b)?(a):(b))
+#define MIN(a,b)   ((a)<(b)?(a):(b))
+
+#define CLEAROBJ(o)   memset(&(o),0,sizeof(o))
+
+#define NNS(str)   ((str)?(str):"")
+
+#define SAFEDELETE(obj)   { if ( obj ) delete obj; obj = NULL; }
+
+
+extern unsigned g_time;
+
+
+class CSysTicks
+{
+  public:
+          static unsigned GetCounter() { return g_time; }
+
+};
