7bd69ff1afd186612c439e9e304bd756cd188d92
sergeys <sergeys@2213d24a-d591-11e5-a0f6-fbcad358b9ab>
2016-11-08 19:12:00 +0000

    config class reorganized

--- EEar/soft/dev/client/config.h
+++ EEar/soft/dev/client/config.h
@@ -3,26 +3,36 @@
 #define __CONFIG_H__
 
 
+class CConfig 
+{
+  public:
+          unsigned short device_id;
+          unsigned char sector;
+          int port_tcp;
+          int port_udp;
+          std::string server;
+          std::string ussd_balance;
+          std::string sms_number;
+          std::string sms_prefix;
+          int gps_baud;
+          bool use_gps;
+          bool use_glonass;
+          bool use_galileo;
+          bool use_beidou;
+          bool debug_mode;
 
-typedef struct {
-unsigned short device_id;
-unsigned char sector;
-int port_tcp;
-int port_udp;
-std::string server;
-std::string ussd_balance;
-std::string sms_number;
-std::string sms_prefix;
-int gps_baud;
-bool use_gps;
-bool use_glonass;
-bool use_galileo;
-bool use_beidou;
-bool debug_mode;
-} TCFG;
+  public:
+          bool ReadConfig();
 
+  private:
+          static bool BuildMap(const char *filename,std::map<std::string,std::string>& map);
+          static void Get(unsigned char &_rc,const std::string& s);
+          static void Get(unsigned short &_rc,const std::string& s);
+          static void Get(int &_rc,const std::string& s);
+          static void Get(bool &_rc,const std::string& s);
+          static void Get(std::string &_rc,const std::string& s);
 
-bool ReadConfig(TCFG &cfg);
+};
 
 
 
