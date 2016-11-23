722897c6c01319b87ef98b53bf31fd92f7752dec
sergeys <sergeys@2213d24a-d591-11e5-a0f6-fbcad358b9ab>
2016-11-23 13:23:03 +0000

    universal async packets sender (inet/SMS/file) added balance requester added

--- EEar/soft/dev/client/def.h
+++ EEar/soft/dev/client/def.h
@@ -8,6 +8,7 @@
 #define CONFIG_FILENAME             "config.cfg"
 #define LOG_FILENAME                "log.txt"
 #define DBG_AUDIO_FILENAME          "record.raw"
+#define OFFLINE_FILENAME            "offline.dat"
 
 #define DEFAULT_SERVER              "195.234.5.137"
 
